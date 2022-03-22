f2e28c9c07e1f25a169144e6a84d70d719c15773
StackSpot Admin <admin@stackspot.com>
2022-03-22 18:42:18 +0000

    Update public repository

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# argorollouts-env-ts-plugin
+# Argo Rollouts plugin
+
+This is a [StackSpot](https://docs.stackspot.com) plugin, based on CDK, that adds [Argo Rollouts Kubernetes Controller](https://argoproj.github.io/argo-rollouts/) to an EKS cluster.
+
+See our [StackSpot EKS stack](https://github.com/stack-spot/zup-eks-env-stack) for more information.
+
+# How to use
+
+Go to application folder and type:
+
+```
+stk apply plugin zup-eks-env-stack/argorollouts-env-ts-plugin
+```
+
+# Prerequisites
+
+This plugin is applicable in applications that were created from the template [https://github.com/stack-spot/eks-env-ts-template](https://github.com/stack-spot/eks-env-ts-template).
---
+++ templates/lib/argorollouts-stack.ts
@@ -0,0 +1,46 @@
+import { NestedStack, NestedStackProps } from "aws-cdk-lib";
+import { Cluster } from "aws-cdk-lib/aws-eks";
+import { Construct } from "constructs";
+import * as yaml from "js-yaml";
+import got from "got";
+
+export class ArgoRolloutsStack extends NestedStack {
+    constructor(scope: Construct, id: string, props: ArgoRolloutsStackProps) {
+        super(scope, id, props);
+
+        (async () => {
+            try {
+
+                const namespace = props.eksCluster.addManifest("argo-rollouts-namespace", {
+                    "apiVersion": "v1",
+                    "kind": "Namespace",
+                    "metadata": {
+                        "name": "argo-rollouts"
+                    }
+                });
+
+                props.eksCluster.addFargateProfile("argo-rollouts-fargate-profile", { selectors: [{ namespace: "argo-rollouts" }] });
+
+                const manifest = await got.get("https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml").text();
+                console.info("Argo Rollouts manifests has been downloaded.");
+                yaml.loadAll(manifest, function (doc) {
+                    const manifest = doc as Record<string, any>;
+                    const kind = manifest["kind"];
+                    const name = manifest["metadata"]["name"];
+
+                    manifest["metadata"]["namespace"] = "argo-rollouts";
+
+                    console.debug("Adding manifest: \n", manifest);
+
+                    props.eksCluster.addManifest(`${name}_${kind}`, doc as Record<string, any>).node.addDependency(namespace);
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }
+}
+
+interface ArgoRolloutsStackProps extends NestedStackProps {
+    eksCluster: Cluster;
+}
