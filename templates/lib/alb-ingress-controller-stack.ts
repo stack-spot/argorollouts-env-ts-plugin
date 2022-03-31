d1279a2d8ef3355ad2ba83c3a792ea944dd7480a
StackSpot Admin <admin@stackspot.com>
2022-03-31 14:48:44 +0000

    Update public repository

--- templates/lib/alb-ingress-controller-stack.ts
+++ templates/lib/alb-ingress-controller-stack.ts
@@ -1,43 +1,44 @@
-import { NestedStack, NestedStackProps, aws_iam } from "aws-cdk-lib";
-import { Cluster } from "aws-cdk-lib/aws-eks";
-import { Construct } from "constructs";
-import * as fs from "fs";
-import { ManifestUtils } from "./common/manifest-utils";
+import { NestedStack, NestedStackProps, aws_iam } from 'aws-cdk-lib';
+import { Cluster } from 'aws-cdk-lib/aws-eks';
+import { Construct } from 'constructs';
+import * as fs from 'fs';
+import { ManifestUtils } from './common/manifest-utils';
 
 export class AlbIngressControllerStack extends NestedStack {
     constructor(scope: Construct, id: string, props: AlbIngressControllerStackProps) {
         super(scope, id, props);
 
-        const ALB_INGRESS_NAME = "alb-ingress-controller";
-        const ALB_INGRESS_NAMESPACE = "kube-system";
+        const ALB_INGRESS_NAME = 'alb-ingress-controller';
+        const ALB_INGRESS_NAMESPACE = 'kube-system';
 
-        ManifestUtils.apply(fs.readFileSync("./manifests/rbac-role.yml", "utf-8"), props.eksCluster, ALB_INGRESS_NAMESPACE);
+        ManifestUtils.apply(fs.readFileSync('./manifests/rbac-role.yml', 'utf-8'), props.eksCluster, ALB_INGRESS_NAMESPACE);
 
-        const policyJsonObj = JSON.parse(fs.readFileSync("./manifests/iam-policy.json", "utf-8"));
-        const policy = new aws_iam.Policy(this, "ALBIngressControllerIAMPolicy", {
+        const policyJsonObj = JSON.parse(fs.readFileSync('./manifests/iam-policy.json', 'utf-8'));
+        const policy = new aws_iam.Policy(this, 'ALBIngressControllerIAMPolicy', {
             document: aws_iam.PolicyDocument.fromJson(policyJsonObj)
         });
 
-        const serviceAccount = props.eksCluster.addServiceAccount(ALB_INGRESS_NAME + "-serviceaccount", {
-            "name": ALB_INGRESS_NAME,
-            "namespace": ALB_INGRESS_NAMESPACE
+        const serviceAccount = props.eksCluster.addServiceAccount(ALB_INGRESS_NAME + '-serviceaccount', {
+            'name': ALB_INGRESS_NAME,
+            'namespace': ALB_INGRESS_NAMESPACE
         });
         serviceAccount.role.attachInlinePolicy(policy);
 
-        props.eksCluster.addHelmChart("alb-ingress-helm-chart", {
-            chart: "aws-load-balancer-controller",
-            repository: "https://aws.github.io/eks-charts",
+        props.eksCluster.addHelmChart('alb-ingress-helm-chart', {
+            chart: 'aws-load-balancer-controller',
+            repository: 'https://aws.github.io/eks-charts',
             namespace: ALB_INGRESS_NAMESPACE,
+            release: 'aws-load-balancer-controller',
             values: {
-                "clusterName": props.eksCluster.clusterName,
-                "vpcId": props.eksCluster.vpc.vpcId,
-                "region": props.eksCluster.env.region,
-                "serviceAccount": {
-                    "create": false,
-                    "name": ALB_INGRESS_NAME
+                'clusterName': props.eksCluster.clusterName,
+                'vpcId': props.eksCluster.vpc.vpcId,
+                'region': props.eksCluster.env.region,
+                'serviceAccount': {
+                    'create': false,
+                    'name': ALB_INGRESS_NAME
                 },
-                "rbac": {
-                    "create": false
+                'rbac': {
+                    'create': false
                 }
             }
         }).node.addDependency(serviceAccount);
--- templates/lib/argorollouts-stack.ts
+++ templates/lib/argorollouts-stack.ts
@@ -1,37 +1,37 @@
-import { NestedStack, NestedStackProps } from "aws-cdk-lib";
-import { Cluster } from "aws-cdk-lib/aws-eks";
-import { Construct } from "constructs";
-import got from "got";
-import { ManifestUtils } from "./common/manifest-utils";
+import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
+import { Cluster } from 'aws-cdk-lib/aws-eks';
+import { Construct } from 'constructs';
+import got from 'got';
+import { ManifestUtils } from './common/manifest-utils';
 
 export class ArgoRolloutsStack extends NestedStack {
     constructor(scope: Construct, id: string, props: ArgoRolloutsStackProps) {
         super(scope, id, props);
 
-        const ARGO_ROLLOUTS_NAMESPACE = "argo-rollouts";
+        const ARGO_ROLLOUTS_NAMESPACE = 'argo-rollouts';
 
         (async () => {
             try {
 
-                const namespace = props.eksCluster.addManifest("argo-rollouts-namespace", {
-                    "apiVersion": "v1",
-                    "kind": "Namespace",
-                    "metadata": {
-                        "name": ARGO_ROLLOUTS_NAMESPACE
+                const namespace = props.eksCluster.addManifest('argo-rollouts-namespace', {
+                    'apiVersion': 'v1',
+                    'kind': 'Namespace',
+                    'metadata': {
+                        'name': ARGO_ROLLOUTS_NAMESPACE
                     }
                 });
 
                 props.eksCluster.addFargateProfile(
-                    "argo-rollouts-fargate-profile",
+                    'argo-rollouts-fargate-profile',
                     {
                         selectors: [{ namespace: ARGO_ROLLOUTS_NAMESPACE }],
-                        fargateProfileName: "argo-rollouts-fargate-profile"
+                        fargateProfileName: 'argo-rollouts-fargate-profile'
                     }
                 );
 
-                const manifest = await got.get("https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml").text();
+                const manifest = await got.get('https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml').text();
 
-                console.info("Argo Rollouts manifests has been downloaded.");
+                console.info('Argo Rollouts manifests has been downloaded.');
 
                 ManifestUtils.apply(manifest, props.eksCluster, ARGO_ROLLOUTS_NAMESPACE, namespace);
 
